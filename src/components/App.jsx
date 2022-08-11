f5617effe46622602ebfbabaa44dc17e561eed75
Kantaro0829 <eshangwan@gmail.com>
2022-08-11 15:13:51 +0900

    [add] 簡易的なリストを追加

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Form from './Form';
+import List from  './List';
 
 // function App() {
 //   return (
@@ -30,6 +31,8 @@ export default class App extends Component {
       <div className="siimple-box siimple--bg-dark">
         <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
         <Form/>
+        <div className="siimple-rule"></div>
+        <List/>
       </div>
     );
   }
